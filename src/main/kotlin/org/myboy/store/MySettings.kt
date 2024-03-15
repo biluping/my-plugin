5a5d8827037952b2cf65d9ef76f9b504dc9f39be
luping.bi <2631132715@qq.com>
2024-03-15 16:48:44 +0800

    docs: add service、state doc

--- src/main/kotlin/org/myboy/store/MySettings.kt
+++ src/main/kotlin/org/myboy/store/MySettings.kt
@@ -10,6 +10,9 @@ import com.intellij.util.xmlb.XmlSerializerUtil
  * 加上了 @Service 就可以通过 val mySettings = service<MySettings>() 直接拿到单例对象
  * @State 配合 PersistentStateComponent 接口用于将数据存储到本地，直接修改属性值后会更新到磁盘
  * 下面示例中，my-setting.xml 文件保存在 build/idea-sandbox/config/options/my-setting.xml，其中 xml 根标签是 my-setting
+ *
+ * https://plugins.jetbrains.com/docs/intellij/persisting-state-of-components.html
+ * https://plugins.jetbrains.com/docs/intellij/plugin-services.html
  */
 @Service
 @State(
