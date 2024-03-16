d34be8d18082eceaa8c33e9192d72dac2399811d
luping.bi <2631132715@qq.com>
2024-03-16 20:20:22 +0800

    feat: 增加 toolwindow 分割线

--- src/main/kotlin/org/myboy/toolwindow/MyToolWindowFactory.kt
+++ src/main/kotlin/org/myboy/toolwindow/MyToolWindowFactory.kt
@@ -4,8 +4,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.openapi.wm.ToolWindowType
+import com.intellij.ui.OnePixelSplitter
 import com.intellij.ui.content.ContentFactory
-import java.awt.FlowLayout
 import javax.swing.JButton
 import javax.swing.JLabel
 import javax.swing.JPanel
@@ -27,17 +27,24 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     private fun createPanel() : JPanel {
         var count = 0
-        val label = JLabel()
+        val label = JLabel("0")
         val btn = JButton("加一")
         btn.addActionListener() {
             label.text = (++count).toString()
         }
 
-        val jPanel = JPanel(FlowLayout())
-        jPanel.add(btn)
-        jPanel.add(label)
+//        val jPanel = JPanel(FlowLayout())
+//        jPanel.add(btn)
+//        jPanel.add(label)
 
-        return jPanel
+        val onePixelSplitter = OnePixelSplitter(true, .98f)
+        onePixelSplitter.dividerWidth = 1
+
+        // 这个 firstComponent 会占据很大上半部分
+        onePixelSplitter.firstComponent = btn
+        onePixelSplitter.secondComponent = label
+
+        return onePixelSplitter
     }
 
 }
